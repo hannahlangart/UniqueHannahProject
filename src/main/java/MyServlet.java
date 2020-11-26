d5e35e0069c6a5410ac88f6974ba76830d6613ef
hannahlangart <hannah.langart18@imperial.ac.uk>
2020-11-26 15:48:07 +0100

    Tutorial 5 Ex 2.1

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,9 +1,12 @@
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
@@ -15,6 +18,10 @@ public class MyServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        Gson gson = new Gson();
+        Patient p=gson.fromJson(reqBody,Patient.class);
+        resp.setContentType("text/html");
+        resp.getWriter().write("Thank you client! "+reqBody);
     }
 }
